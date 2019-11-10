276e83bffdc2463c21ff47defe5053f2c08a0bcb
Amit Raj <amitraj200raj@gmail.com>
2019-11-10 19:48:01 +0530

    login file updated

--- login.php
+++ login.php
@@ -2,18 +2,21 @@
 include_once 'header.php';
 include_once 'includes/dbh.inc.php';
 ?>
- 
-        <div class="container">
+ <br><br><br><br><br>
+        <div class="container-fluid">
             <div class="row">
-                <div class="col-md-12 col-sm-12">
+                <div class= "col-md-4 col-sm-12"></div>
+                <div class="col-md-4 col-sm-12">
                     <div class="login">
-                    <br><br><br>
                         <h2>Login</h2>
                         <form action="includes/login.inc.php" method="POST">
                         <input type ="text" name ="user" placeholder=" Username" id="log-user">
                         <input type ="password" name ="pwd" placeholder="Password" id="log-user">
                         <center><button type ="submit" name ="submit" class="musica-btn">Login</button></center>
                         </form>
+                        <br><br>
+                        <p class= "no-log" style="color:#fff;"><a href="signup.php">Don't have an account?</a></p>
+                        <p class= "no-log" style="color:#fff;">Forgot Password !!</p>
                     </div>
                 </div>
             </div>
